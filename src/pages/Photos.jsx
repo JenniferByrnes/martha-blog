bd6ea767b33911879507b02a95536687865a81e8
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2023-02-04 21:40:07 -0500

    Changed Instagram pointers

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import SubNav from '../components/SubNav';
 import Gallery from '../components/Gallery'
+import { SocialIcon } from 'react-social-icons';
 
 export default function Photos() {
 
@@ -18,25 +19,28 @@ export default function Photos() {
   return (
     <div>
       <SubNav
+
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
         contactSelected={contactSelected}
         setContactSelected={setContactSelected}
       ></SubNav>
-      <main className="container mx-auto p-6" >
-        {( currentCategory.categoryName === "all")
+      <SocialIcon url="https://www.instagram.com/blueridge.hideaway/" bgColor="#546a7b" style={{ height: 25, width: 25 }} />
+      <main className="container mx-auto px-6" >
+
+        {(currentCategory.categoryName === "all")
           ?
           <>
-          {/* TODO - make this work with loop - somehow get useState to work here */}
-          {/* The following statement causes a re-render so that only the last category is shown */}
-          {/* {setCurrentCategory(categories[1])} */}
-          <Gallery currentCategory={{ }}></Gallery>
-            <Gallery currentCategory={{categoryName: "outside", description: "Your breathtaking view from the house"}}></Gallery>
-            <Gallery currentCategory={{categoryName: "living", description: "Well appointed main living spaces"}}></Gallery>
+            {/* TODO - make this work with loop - somehow get useState to work here */}
+            {/* The following statement causes a re-render so that only the last category is shown */}
+            {/* {setCurrentCategory(categories[1])} */}
+            <Gallery currentCategory={{}}></Gallery>
+            <Gallery currentCategory={{ categoryName: "outside", description: "Your breathtaking view from the house" }}></Gallery>
+            <Gallery currentCategory={{ categoryName: "living", description: "Well appointed main living spaces" }}></Gallery>
 
-            <Gallery currentCategory={{categoryName: "bedrooms", description: "Photos of the three beautifully decorated bedrooms"}}></Gallery>
-            <Gallery currentCategory={{categoryName: "other", description: "Pictures that defy categorization."}}></Gallery>
+            <Gallery currentCategory={{ categoryName: "bedrooms", description: "Photos of the three beautifully decorated bedrooms" }}></Gallery>
+            <Gallery currentCategory={{ categoryName: "other", description: "Pictures that defy categorization." }}></Gallery>
           </>
           : <Gallery currentCategory={currentCategory}></Gallery>
         }
