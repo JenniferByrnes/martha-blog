85b769ac8022a3310b9b45340774ee17a198abbe
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2023-02-05 16:54:23 -0500

    name change and new text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,12 @@ const Home = () => {
 
   return (
     // Page Container
-    <section className=" mx-auto h-screen w-screen bg-pcGreen" >
+    <section className=" mx-auto min-h-full h-screen w-screen bg-pcGreen" >
       {/* Divide the container into one flex column */}
       <div className="flex flex-col  p-6 justify-center items-center" >
 
         <p className="text-4xl md:text-6xl text-center mb-2">
-          Whatever you want here
+          Burfmusing
         </p>
         {/* Card */}
         <div className="rounded-2xl shadow-lg max-w-sm bg-white">
@@ -22,8 +22,8 @@ const Home = () => {
             />
           </div>
           {/* Card Info below image */}
-          <div className="p-6">
-            <p className="text-xl font-medium text-center mb-2">
+          {/* <div className="p-6"> */}
+            {/* <p className="text-xl font-medium text-center mb-2">
               Home Page Title
             </p>
             <p
@@ -33,11 +33,11 @@ const Home = () => {
             </p>
             <p className="text-center leading-5 tracking-wide">
               Some quick example text to build on the card title and make up the bulk of the card‘s content.
-            </p>
-          </div>
+            </p> */}
+          {/* </div> */}
         </div>
-        <p className="italic font-bold text-4xl md:text-6xl text-rose-400 text-center mt-4">
-          Pictures, text, whatever
+        <p className="italic font-bold text-l sm:text-xl md:text-3xl text-rose-400 text-center mt-4">
+        Burfmusing is where Martha Burford’s love for hospitality, music, mountains, people, seasons, and purpose come together. Here you can check out the latest news for her AirBnB, Blue Ridge Hideaway; follow links to music Martha loves and makes with others; and read her blog for musings on dachshunds, mourning, falling in love with sunsets, and the connections we discover when we listen without reservation and open our hearts. 
         </p>
       </div>
     </section>
